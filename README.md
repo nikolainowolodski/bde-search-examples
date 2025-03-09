6b2fea156adf2e56d6a19dffa306ef11452ea2ed
Niko <niko@NN-MB-Pro.fritz.box>
2025-03-09 19:10:23 +0100

    Update readme, remove useMemo from the react app

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # bde-search-examples
-Simple one page search examples with react and nuxt like on bde
+
+Simple one page search examples with react and nuxt like on bde. This was done
+using react/typescript with vite and nuxt. Both projects are pretty much the same logic wise.
+
+### Why both?
+
+I had fun doing it in react and since it was mentioned that doing it in nuxt would be appreciated, I dipped my feet into nuxt a little and tried to reproduce it.
+
+## How to run
+
+### For the react app
+
+- `cd bde-search-react`
+- `npm install`
+- `npm run dev`
+- The app will run on `http://localhost:5173/`
+
+### For the nuxt app
+
+- `cd bde-search-nuxt`
+- `npm install`
+- `npm run dev`
+- The app will run on `http://localhost:3000/`
--- bde-search-react/src/App.tsx
+++ bde-search-react/src/App.tsx
@@ -1,41 +1,30 @@
-import { useState, useMemo, ChangeEvent } from "react";
+import { useState, ChangeEvent } from "react";
 import products from "../src/top-1000-products.json";
-
-// define product type
-type Product = {
-  name: string;
-  category: string;
-};
 
 export const App = () => {
   // state for user search tracking
   const [userSearchQuery, setUserSearchQuery] = useState("");
 
   // Filter products based on search query
-  const productSearchResults = useMemo(() => {
-    if (!userSearchQuery) return [] as Product[];
-    return products.filter((product: Product) =>
-      product.name.toLowerCase().includes(userSearchQuery.toLowerCase())
-    );
-  }, [userSearchQuery]);
+  const productSearchResults = !userSearchQuery
+    ? []
+    : products.filter((product) =>
+        product.name.toLowerCase().includes(userSearchQuery.toLowerCase())
+      );
 
-  // List of all categories from matching products
-  const categorySearchResults = useMemo(() => {
-    const categories = productSearchResults.map((product) => product.category);
-    // Make sure to return unique categories and get rid of dupes
-    return Array.from(new Set(categories));
-  }, [productSearchResults]);
+  // Return an array of unique categories
+  const categorySearchResults = Array.from(
+    new Set(productSearchResults.map((product) => product.category))
+  );
 
   // Cheap trick to show some type of "Suchvorschlag"
-  const searchSuggestions = useMemo(() => {
-    if (!userSearchQuery) return [] as string[];
-    // If we got products related to the search query, just show the first product as search result
-    // Just for the sake of showing something, in a live production I would obviously try to show a more relevant product ;)
-    if (productSearchResults.length > 0) {
-      return [productSearchResults[0].name];
-    }
-    return [];
-  }, [userSearchQuery, productSearchResults]);
+  // If we got products related to the search query, just show the first product as search result
+  // Just for the sake of showing something, in a live production I would obviously try to show a more relevant product ;)
+  const searchSuggestions = !userSearchQuery
+    ? []
+    : productSearchResults.length > 0
+    ? [productSearchResults[0].name]
+    : [];
 
   // Update the search result whenever the user types something through our setter function
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
